6480a981a5fc3361826afbe8c84aa6322f1bb259
Putu Chandra <chandevcode@gmail.com>
2022-09-13 10:26:45 +0800

    done crud

---
+++ pkg/books/add_book.go
@@ -0,0 +1,34 @@
+package books
+
+import (
+	"net/http"
+
+	"github.com/chanlevelup/go_gin_api/pkg/common/models"
+	"github.com/gin-gonic/gin"
+)
+
+type AddBookRequestBody struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+}
+
+func (h handler) AddBook(c *gin.Context) {
+	body := AddBookRequestBody{}
+
+	// getting request body
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	var book models.Book
+	book.Title = body.Title
+	book.Author = body.Author
+	book.Description = body.Description
+
+	if result := h.DB.Create(&book); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+}
